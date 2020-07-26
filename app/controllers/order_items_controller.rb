42a9e44a2e8d8bedab261be6714f266beb847657
marshall nyasha chikari <marshall.chikari@gmail.com>
2020-07-27 01:34:33 +0200

    fixed error

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -3,7 +3,7 @@ class OrderItemsController < ApplicationController
     @order = current_order
     @order_item = @order.order_items.new(order_item_params)
     respond_to do |format|
-      if @order.save
+      if @order_item.save
         format.js { render js: "window.top.location.reload(true);" }
       end
     end
