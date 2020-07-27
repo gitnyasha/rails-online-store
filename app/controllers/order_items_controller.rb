2c8c70119ce73d92e8cf517ea1ccb1dfdd3022fc
marshall nyasha chikari <marshall.chikari@gmail.com>
2020-07-27 02:40:28 +0200

    .

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -2,7 +2,7 @@ class OrderItemsController < ApplicationController
   def create
     @order = current_order
     @order_item = @order.order_items.new(order_item_params)
-    @order_item.save
+    @order.save
     redirect_to carts_path
     session[:order_id] = @order.id
   end
