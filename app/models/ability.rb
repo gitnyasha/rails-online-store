e6c54adddb7bed3cbb626bfdc952e3a18db5ffa9
marshall nyasha chikari <marshall.chikari@gmail.com>
2020-07-27 03:25:58 +0200

    .

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -7,6 +7,7 @@ class Ability
     # Define abilities for the passed in user here. For example:
     #
     user ||= User.new # guest user (not logged in)
+    can :manage, :all
     if user.admin_role?
       can :manage, :all
     else
